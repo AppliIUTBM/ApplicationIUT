beee14b40f7b5b4d0cecf0e117e5bb8cfd0dda5c
rjeannou <romain.jeannoutot@gmail.com>
2015-02-04 18:59:37 +0100

    add menu in edt

--- app/src/main/java/com/iutbm/applicationiut/EDTActivity.java
+++ app/src/main/java/com/iutbm/applicationiut/EDTActivity.java
@@ -1,9 +1,11 @@
 package com.iutbm.applicationiut;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -162,5 +164,29 @@ public class EDTActivity extends Activity implements RefreshUI {
             asyncTask.execute();
         }
     };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.calendar_action) {
+            Intent intent = new Intent(this,ConfigEDTActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
